4b6d1ec996dc4a71610824e328942c4bc4d1c8c8
hgjersdal <hgjersdal@gmail.com>
2017-12-08 16:50:29 +0100

    Added start_ioc_gen.sh

--- st_gen.py
+++ st_gen.py
@@ -10,6 +10,7 @@ ccs100 = False
 PM100 = False
 LPR59 = False
 
+# Check for camera
 arv_output = str(subprocess.check_output(['arv-tool-0.4', 'control', 'DeviceModelName']))
 if(not(arv_output.find('235B') == -1)):
     manta235 = True
@@ -17,6 +18,7 @@ if(not(arv_output.find('235B') == -1)):
 if(not(arv_output.find('125B') == -1)):
     manta125B = True
 
+# Check for known USB devices
 for bus in usb.busses():
     for dev in bus.devices:
         if(dev.idVendor == 4883 and dev.idProduct == 32882):
@@ -33,15 +35,15 @@ def dump_file_to_file(in_name, out_file):
 
 
 with open('/tmp/st_gen.cmd', 'w') as f:
-    dump_file_to_file('st_components/init.txt', f)
+    dump_file_to_file('/home/dev/git/havards_stuff/st_components/init.txt', f)
     if(manta125):
-        dump_file_to_file('st_components/manta125.txt', f)
+        dump_file_to_file('/home/dev/git/havards_stuff/st_components/manta125.txt', f)
     if(manta235):
-        dump_file_to_file('st_components/manta235.txt', f)
+        dump_file_to_file('/home/dev/git/havards_stuff/st_components/manta235.txt', f)
     if(ccs100):
-        dump_file_to_file('st_components/ccs100.txt', f)
+        dump_file_to_file('/home/dev/git/havards_stuff/st_components/ccs100.txt', f)
     if(PM100):
-        dump_file_to_file('st_components/PM100.txt', f)
+        dump_file_to_file('/home/dev/git/havards_stuff/st_components/PM100.txt', f)
     if(LPR59):
-        dump_file_to_file('st_components/LPR59.txt', f)
-    dump_file_to_file('st_components/tail.txt', f)
+        dump_file_to_file('/home/dev/git/havards_stuff/st_components/LPR59.txt', f)
+    dump_file_to_file('/home/dev/git/havards_stuff/st_components/tail.txt', f)
---
+++ start_ioc_gen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python ~/git/haavards_stuff/st_gen.py
+source $HOME/bin/ng3esetup.sh
+which caget >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "caget not found in PATH!"
+	exit 1
+fi
+
+cd $NG3E_IOCS/imgioc-master+4/iocBoot/iocImg || exit 1
+../../bin/linux-x86_64/imgApp /tmp/st_gen.cmd
